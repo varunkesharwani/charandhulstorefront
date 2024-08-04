9eabead4183064daf7234739b5d903c5ec2e03d5
varunkesharwani <72571998+varunkesharwani@users.noreply.github.com>
2024-08-05 04:24:17 +0530

    Update index.tsx

--- src/modules/layout/templates/topnav/index.tsx
+++ src/modules/layout/templates/topnav/index.tsx
@@ -10,7 +10,7 @@ export const TopHeader: React.FC = () => {
           <span>Pure Copper Utensils</span>
           <span>Additional Promotion Here</span>
           <span>New Arrivals: Check Out the Latest Collection</span>
-          <span>Limited Time Offer: 10% Off on All Orders</span>
+          <span>Limited Time Offer: 20% Off on All Orders</span>
           <span>Subscribe Now and Get Exclusive Deals</span>
           {/* Duplicate the text content to ensure seamless scrolling */}
           <span>Free Shipping Above 5000</span>

e96700da39999b5b15f37814c39bec55e83e1d63
shubhamchoudhary-2003 <shubhamera484@gmail.com>
2024-09-02 10:50:51 +0530

    changes

--- src/modules/checkout/components/payment-button/index.tsx
+++ src/modules/checkout/components/payment-button/index.tsx
@@ -13,14 +13,11 @@ import Spinner from "@modules/common/icons/spinner"
 import useRazorpay, { RazorpayOptions } from "react-razorpay"
 
 type PaymentButtonProps = {
-  cart: Omit<Cart, "refundable_amount" | "refunded_total">
-  "data-testid": string
+  cart: Omit<Cart, "refundable_amount" | "refunded_total">,
+  'data-testid': string
 }
 
-const PaymentButton: React.FC<PaymentButtonProps> = ({
-  cart,
-  "data-testid": dataTestId,
-}) => {
+const PaymentButton: React.FC<PaymentButtonProps> = ({ cart, 'data-testid': dataTestId }) => {
   const notReady =
     !cart ||
     !cart.shipping_address ||
@@ -32,29 +29,26 @@ const PaymentButton: React.FC<PaymentButtonProps> = ({
 
   const paymentSession = cart.payment_session as PaymentSession
 
+
   switch (paymentSession.provider_id) {
     case "razorpay":
-      return (
-        <RazorpayPaymentButton
-          notReady={notReady}
-          cart={cart}
-          session={paymentSession}
-          data-testid={dataTestId}
-        />
-      )
-    case "manual":
-      return
-    case "paypal":
-      return
-    default:
-      return <Button disabled>Select a payment method</Button>
+      return <RazorpayPaymentButton notReady={notReady} cart={cart} session={paymentSession} data-testid={dataTestId} />
+    // case "manual":
+    //   return <ManualTestPaymentButton notReady={notReady} data-testid={dataTestId} />
+    // case "paypal":
+    //   return <PayPalPaymentButton notReady={notReady} cart={cart} data-testid={dataTestId} />
+    // default:
+    //   return <Button disabled>Select a payment method</Button>
   }
 }
+
+
+
 
 const RazorpayPaymentButton = ({
   session,
   notReady,
-  cart,
+  cart
 }: {
   session: PaymentSession
   notReady: boolean
@@ -62,10 +56,8 @@ const RazorpayPaymentButton = ({
 }) => {
   const [disabled, setDisabled] = useState(false)
   const [submitting, setSubmitting] = useState(false)
-  const [errorMessage, setErrorMessage] = useState<string | undefined>(
-    undefined
-  )
-  const [Razorpay] = useRazorpay()
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined)
+  const [Razorpay] = useRazorpay();
 
   const orderData = session.data as Record<string, string>
   const onPaymentCompleted = async () => {
@@ -75,17 +67,18 @@ const RazorpayPaymentButton = ({
     })
   }
 
+
   const handlePayment = useCallback(() => {
     const options: RazorpayOptions = {
       callback_url: `${process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL}/razorpay/hooks`,
-      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY ?? "",
+      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY ?? '',
       amount: session.amount.toString(),
       order_id: orderData.id,
       currency: cart.region.currency_code.toLocaleUpperCase(),
-      name: process.env.COMPANY_NAME ?? "Charandhul ",
+      name: process.env.COMPANY_NAME ?? "ShopNTrolly ",
       description: `Order number ${orderData.id}`,
 
-      image: "https://res.cloudinary.com/dgvwg1ikm/image/upload/v1719594807/vkcustoby50gjjiwhqqc.jpg",
+      image: "https://example.com/your_logo",
       modal: {
         backdropclose: true,
         escape: true,
@@ -99,32 +92,38 @@ const RazorpayPaymentButton = ({
       handler: async (args) => {
         onPaymentCompleted()
       },
-      prefill: {
-        name:
-          cart?.billing_address.first_name +
-          " " +
-          cart?.billing_address.last_name,
-        email: cart?.email,
-        contact: (cart?.billing_address?.phone ||
-          cart?.shipping_address?.phone) as string,
+      "prefill": {
+        "name": cart?.billing_address.first_name + " " + cart?.billing_address.last_name  ,
+        "email": cart?.email,
+        "contact":( cart?.billing_address?.phone || cart?.shipping_address?.phone) as string
       },
-      notes: {
-        address: cart?.billing_address,
-        order_notes: session.data.notes,
+      "notes": {
+        "address": cart?.billing_address,
+        "order_notes": session.data.notes
       },
-    }
+      
+    };
 
-    const razorpay = new Razorpay(options)
-    razorpay.open()
+    const razorpay = new Razorpay(options);
+    razorpay.open();
     razorpay.on("payment.failed", function (response: any) {
       setErrorMessage(JSON.stringify(response.error))
     })
-    razorpay.on("payment.authorized", function (response: any) {})
-    razorpay.on("payment.captured", function (response: any) {})
-  }, [Razorpay, cart?.billing_address, cart?.email, cart.region.currency_code, cart?.shipping_address?.phone, orderData.id, session.amount, session.data.notes])
+    razorpay.on("payment.authorized", function (response: any) {
+
+    })
+    razorpay.on("payment.captured", function (response: any) {
+
+    }
+    )
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Razorpay]);
   return (
     <>
-      <Button disabled={submitting || notReady} onClick={handlePayment} className="mt-6 bg-[#ffc600] hover:bg-[#ffc600]/40 text-black">
+      <Button
+        disabled={submitting || notReady}
+        onClick={handlePayment}
+      >
         {submitting ? <Spinner /> : "Place Your Order"}
       </Button>
       {errorMessage && (

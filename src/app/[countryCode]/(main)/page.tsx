425ba157ff9537162954814046f76e06777b3e50
shubhamchoudhary-2003 <shubhamera484@gmail.com>
2024-09-14 15:14:21 +0530

    static blog

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -18,6 +18,48 @@ export const metadata: Metadata = {
     "Charandhul Store is a curated collection of Copper Utlis.",
 }
 
+// const getCollectionsWithProducts = cache(
+//   async (
+//     countryCode: string
+//   ): Promise<ProductCollectionWithPreviews[] | null> => {
+//     const { collections } = await getCollectionsList(0, 5)
+
+//     if (!collections) {
+//       return null
+//     }
+
+//     const collectionIds = collections.map((collection) => collection.id)
+
+//     await Promise.all(
+//       collectionIds.map((id) =>
+//         getProductsList({
+//           queryParams: { collection_id: [id] },
+//           countryCode,
+//         })
+//       )
+//     ).then((responses) =>
+//       responses.forEach(({ response, queryParams }) => {
+//         let collection
+
+//         if (collections) {
+//           collection = collections.find(
+//             (collection) => collection.id === queryParams?.collection_id?.[0]
+//           )
+//         }
+
+//         if (!collection) {
+//           return
+//         }
+
+//         collection.products = response.products as unknown as Product[]
+//       })
+//     )
+
+//     return collections as unknown as ProductCollectionWithPreviews[]
+//   }
+// )
+
+
 const getCollectionsWithProducts = cache(
   async (
     countryCode: string
@@ -55,9 +97,17 @@ const getCollectionsWithProducts = cache(
       })
     )
 
-    return collections as unknown as ProductCollectionWithPreviews[]
+    // Sort collections based on `isFeatured` metadata (as a number)
+    const sortedCollections = collections.sort((a, b) => {
+      const isFeaturedA = parseInt(a.metadata?.isPriorityFeatured?.toString() || "0", 10)
+      const isFeaturedB = parseInt(b.metadata?.isPriorityFeatured?.toString() || "0", 10)
+      return isFeaturedA - isFeaturedB
+    })
+
+    return sortedCollections as unknown as ProductCollectionWithPreviews[]
   }
 )
+
 
 export default async function Home({
   params: { countryCode },

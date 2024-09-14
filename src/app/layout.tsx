e744fcd22be6fd50718477f533ffb40359bc7636
shubhamchoudhary-2003 <shubhamera484@gmail.com>
2024-09-14 17:53:11 +0530

    adding website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Cabin } from "next/font/google"
 import { Metadata } from "next"
 import "styles/globals.css"
 import Head from "next/head"
+import { GoogleTagManager } from '@next/third-parties/google' 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 const playfair_display = Playfair_Display({
   subsets: ["latin"],
@@ -21,11 +22,27 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
-      <Head>
-
-        <meta name="ahrefs-site-verification" content="db5d224af879d6b9700c1939a1b8abbcd971b1dcfa6559d21b2ff63b4fa06e79" />
-      </Head>
+      <GoogleTagManager gtmId="GTM-PWGQ6326" />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-TEXCCJ1GF6"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TEXCCJ1GF6');
+            `,
+        }}
+      />
       <body className={playfair_display.variable + " " + cabin.variable}>
+
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWGQ6326"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         <main className="relative font-cabin ">{props.children}</main>
       </body>
     </html>
